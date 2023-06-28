7fc15d4390f96eb0d8a94880db75acafac815af4
Davide <Davide@DESKTOP-JU17G44.homenet.telecomitalia.it>
2023-06-28 17:56:25 +0200

    added method

--- src/main/java/it/generationitaly/events/repository/EventoRepository.java
+++ src/main/java/it/generationitaly/events/repository/EventoRepository.java
@@ -1,7 +1,11 @@
 package it.generationitaly.events.repository;
 
+import java.util.List;
+
 import it.generationitaly.events.entity.Evento;
 
 public interface EventoRepository extends JpaRepository<Evento, Integer> {
+	
+	List<Evento> findByTagId(int id);
 
 }
--- src/main/java/it/generationitaly/events/repository/impl/EventoRepositoryImpl.java
+++ src/main/java/it/generationitaly/events/repository/impl/EventoRepositoryImpl.java
@@ -1,8 +1,39 @@
 package it.generationitaly.events.repository.impl;
 
-import it.generationitaly.events.repository.EventoRepository;
+import java.util.ArrayList;
+import java.util.List;
+
 import it.generationitaly.events.entity.Evento;
+import it.generationitaly.events.repository.EventoRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.PersistenceException;
 
 public class EventoRepositoryImpl extends JpaRepositoryImpl<Evento, Integer> implements EventoRepository {
+
+	@Override
+	public List<Evento> findByTagId(int id) {
+		EntityManager em = null;
+		EntityTransaction tx = null;
+		List<Evento> eventi = new ArrayList<Evento>();
+		try {
+			em = emf.createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+			eventi = em.createQuery("select e from Evento e join fetch p.tagEvento where p.tagEvento = :id", Evento.class)
+					.setParameter("id", id).getResultList();
+			tx.commit();
+		} catch (PersistenceException e) {
+			if (tx != null && tx.isActive())
+				tx.rollback();
+			System.err.println(e.getMessage());
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+		return eventi;
+
+	}
 
 }
