70497273f053ded9ae414d511063349a207c0777
Glo <glorialocurto@hotmail.it>
2023-06-28 11:38:31 +0200

    created repository

---
+++ src/main/java/it/generationitaly/events/repository/impl/UserRepositoryImpl.java
@@ -0,0 +1,8 @@
+package it.generationitaly.events.repository.impl;
+
+import it.generationitaly.events.entity.User;
+import it.generationitaly.events.repository.UserRepository;
+
+public class UserRepositoryImpl extends JpaRepositoryImpl<User, Integer> implements UserRepository {
+
+}
