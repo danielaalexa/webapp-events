0f05070892ac394c602b1d4cbb8384ca103a8ff3
Davide <Davide@DESKTOP-JU17G44.homenet.telecomitalia.it>
2023-07-04 09:45:59 +0200

    fix

--- src/main/java/it/generationitaly/events/controller/ServletSearchByDateAndCitta.java
+++ src/main/java/it/generationitaly/events/controller/ServletSearchByDateAndCitta.java
@@ -17,7 +17,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/ServletSearchByDateAndCitta")
+@WebServlet("/servletSearchByDateAndCitta")
 public class ServletSearchByDateAndCitta extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
