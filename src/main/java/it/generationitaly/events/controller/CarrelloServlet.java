2b40897169fe1bd3a7ba77bd381e327682b18466
lexad <lexad@192.168.0.30>
2023-07-04 17:04:10 +0200

    commit

--- src/main/java/it/generationitaly/events/controller/CarrelloServlet.java
+++ src/main/java/it/generationitaly/events/controller/CarrelloServlet.java
@@ -1,6 +1,7 @@
 package it.generationitaly.events.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import it.generationitaly.events.entity.Evento;
@@ -26,30 +27,41 @@ public class CarrelloServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		List<Prenotazione> prenotazioni = new ArrayList<Prenotazione>();
 		HttpSession session = request.getSession(); // request.getSession(false);
-		// List<Prenotazione> prenotazioni = (List<Prenotazione>)
-		// session.getAttribute("prenotazioni");
 		User user = (User) session.getAttribute("user");
 		System.out.println(user);
 		if (user == null) {
 			response.sendRedirect("login.jsp");
 		} else {
-			List<Prenotazione> prenotazioni = user.getPrenotazioni();
 			int id = Integer.parseInt(request.getParameter("id"));
 			Evento evento = eventoRepository.findById(id);
-			System.out.println(evento);
 			Prenotazione prenotazione = new Prenotazione();
 			prenotazione.setUser(user);
 			prenotazione.setEvento(evento);
-			System.out.println(prenotazione);
 			prenotazioneRepository.save(prenotazione);
 			prenotazioni.add(prenotazione);
+			user.setPrenotazioni(prenotazioni);
 			session.setAttribute("prenotazioni", prenotazioni);
+			request.setAttribute("prenotazioni", prenotazioni);
 			request.getRequestDispatcher("carrello.jsp").forward(request, response);
 		}
-
+		
+		if (request.getParameter("quantita") != null) {
+			getQuantita(request, response);
+			return;
+		}
 	}
 
+	protected void getQuantita(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		int idPrenotazione = Integer.parseInt(request.getParameter("idPrenotazione"));
+		Prenotazione prenotazione = prenotazioneRepository.findById(idPrenotazione);
+		int quantita = Integer.parseInt(request.getParameter("quantita"));
+		prenotazione.setQuantita(quantita);
+		prenotazioneRepository.update(prenotazione);
+	}
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		getBuy(request, response);
