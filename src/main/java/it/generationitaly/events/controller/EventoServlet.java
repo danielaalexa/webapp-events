75cdff9b7e565282ada623bcd6d0046a23e71856
Glo <glorialocurto@hotmail.it>
2023-07-06 17:04:27 +0200

    info evento aggoirnato

--- src/main/java/it/generationitaly/events/controller/EventoServlet.java
+++ src/main/java/it/generationitaly/events/controller/EventoServlet.java
@@ -22,22 +22,61 @@ public class EventoServlet extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		HttpSession session = request.getSession(); // request.getSession(false);
+		// HttpSession session = request.getSession(); // request.getSession(false);
+		System.out.println("siamo dentro la servlet");
 		String idAsString = request.getParameter("id");
 		if (idAsString != null) {
-			int id = Integer.parseInt(idAsString);
-			Evento evento = eventoRepository.findById(id);
-			request.setAttribute("evento", evento);
-			RequestDispatcher requestDispatcher = request.getRequestDispatcher("evento.jsp");
-			requestDispatcher.forward(request, response);
+			getEventoInfo(request, response);
+			getCards(request, response);
+			System.out.println("siamo fuori");
 			return;
 		}
 		
+		
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	private void getCards(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		List<Integer> idCards = randomId();
 
+		int id1 = idCards.get(1);
+		System.out.println(id1);
+		int id2 = idCards.get(2);
+		System.out.println(id2);
+
+		Evento evento1 = eventoRepository.findById(id1);
+		Evento evento2 = eventoRepository.findById(id2);
+		System.out.println(evento1.getNome());
+		System.out.println(evento2.getNome());
+
+		request.setAttribute("evento1", evento1);
+		request.setAttribute("evento2", evento2);
+		request.getRequestDispatcher("evento.jsp").forward(request, response);
 	}
+	
+	private void getEventoInfo(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String idAsString = request.getParameter("id");
+		System.out.println(idAsString);
+		int id = Integer.parseInt(idAsString);
+		Evento evento = eventoRepository.findById(id);
+		request.setAttribute("evento", evento);
+		request.getRequestDispatcher("evento.jsp");
+		
+	}
+	
+	private static List<Integer> randomId() {
 
+		List<Integer> ids = new ArrayList<Integer>();
+		for (int i = 0; i < 3; i++) {
+			int add = (int) (Math.random() * (10 - 1 + 1)) + 1;
+			while (ids.contains(add)) {
+				add = (int) (Math.random() * (10 - 1 + 1)) + 1;
+			}
+
+			ids.add(add);
+
+		}
+		return ids;
+	}
 }
